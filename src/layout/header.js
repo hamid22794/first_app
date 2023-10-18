58412e517e33e8ab6480b988f6814e76cfe9d3d0
hamid22794 <sm.hamid22794@gmail.com>
2023-10-18 23:00:27 +0500

    Services dropdown completed

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,23 +1,61 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
   return (
     <header>
-    <div className="page_width">
-      <div className="nav_desktop">
-       <div className="logo"><img src={props.logo} alt="" /></div>
-       <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/services">Services</Link></li>
-          <li><Link to="/contact-us">Contact Us</Link></li>
-          <li><a href="tel:+971586747123">{props.icon}<span>+971 58 67 47 123</span></a></li>
-        </ul>
-       </nav>
-    </div>  
-    </div>   
+      <div className="page_width">
+        <div className="nav_desktop">
+          <div className="logo">
+            <img src={props.logo} alt="" />
+          </div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li className="services_dropdown">
+                <Link to="/services">Services {props.icon1}</Link>
+                <div className="services_submenu">
+                  <ul>
+                    <li>
+                      <a href="/">AC Services</a>
+                    </li>
+                    <li>
+                      <a href="/">Duct Cleaning</a>
+                    </li>
+                    <li>
+                      <a href="/">Handyman</a>
+                    </li>
+                    <li>
+                      <a href="/">Plumbing</a>
+                    </li>
+                    <li>
+                      <a href="/">Electrical</a>
+                    </li>
+                    <li>
+                      <a href="/">Painting</a>
+                    </li>
+                    <li>
+                      <a href="/">Water Tank Cleaning</a>
+                    </li>
+                  </ul>
+                </div>
+              </li>
+              <li>
+                <Link to="/contact-us">Contact Us</Link>
+              </li>
+              <li className="tel">
+                <a href="tel:+971586747123">
+                  {props.icon}
+                  <span>+971 58 67 47 123</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
     </header>
-  )
-}
-export default Header
+  );
+};
+export default Header;
--- src/layout/index.js
+++ src/layout/index.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from './header'
 import Footer from './footer'
 import {BsFillTelephoneFill} from "react-icons/bs"
+import {AiOutlineDown} from "react-icons/ai"
 import logoImg from "../images/logo/logo.svg"
 
 const Layout = ({children}) => {
   return (
     <>
-    <Header logo={logoImg} icon = {<BsFillTelephoneFill />} />
+    <Header logo={logoImg} icon = {<BsFillTelephoneFill />} icon1 = {<AiOutlineDown color='#C4C4C4' />}/>
     {children}
     <Footer logo={logoImg} icons = {<BsFillTelephoneFill />} />
     </>
