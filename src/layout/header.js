e36ced4f9f3d198d4aaae76cc783c36c4043132c
hamid22794 <sm.hamid22794@gmail.com>
2023-10-19 23:08:16 +0500

    Homepage responsive & work  on mobile nav

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Header = (props) => {
+  const [show, setShow] = useState(false);
+  const showNav = () => {
+    document.getElementById("mobile_id").style.display = "block";
+  };
+  const hideNav = () => {
+    document.getElementById("mobile_id").style.display = "none";
+  };
   return (
     <header>
       <div className="page_width">
         <div className="nav_desktop">
+          <div className="menu_bar" onClick={showNav}>
+            <FaBars />
+          </div>
           <div className="logo">
             <a href="/">
               <img src={props.logo} alt="" />
@@ -55,6 +66,40 @@ const Header = (props) => {
               </li>
             </ul>
           </nav>
+          <div className="mobile_nav" id="mobile_id">
+            <div className="icon">
+              <FaTimes color="white" onClick={hideNav} />
+              <ul>
+                <li>
+                  <a href="/">Home</a>
+                </li>
+                <li>
+                  <a href="/services">Services</a>
+                </li>
+                <li>
+                  <a href="/services">AC Services</a>
+                </li>
+                <li>
+                  <a href="/">Duct Cleaning</a>
+                </li>
+                <li>
+                  <a href="/">Handyman</a>
+                </li>
+                <li>
+                  <a href="/">Plumbing</a>
+                </li>
+                <li>
+                  <a href="/">Electrical</a>
+                </li>
+                <li>
+                  <a href="/">Painting</a>
+                </li>
+                <li>
+                  <a href="/">Water Tank Cleaning</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </header>
